eab7c7fdeb9483ff4378a0b7b1baeaea3c7122c7
Rauliqbal <hgostline.gt@gmail.com>
2022-09-06 22:10:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <router-view v-slot="{Component}">
+    <transition name="fade" mode="out-in">      
+      <component :is="Component"/>
+    </transition>
+    <Footer/>
+  </router-view>
+</template>
+
+
+<style type="text/css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+Footer
