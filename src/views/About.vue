69f48b391f7f3de8d56c67af58a1ee8f7221a83e
Raul iqbal <89615372+Rauliqbal@users.noreply.github.com>
2022-09-07 08:18:30 +0700

    Update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    About
+  <div class="flex items-center justify-center  mt-8">
+   <h1 class="text-5xl text-slate-900 hover:text-blue-500 font-bold">About view</h1>
   </div>
 </template>
 
